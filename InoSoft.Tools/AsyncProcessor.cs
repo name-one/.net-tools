5c88a7697285e0627f980b56842a1a630b6a9349
Alexey Ostrovsky <d4ruko@gmail.com>
2012-07-06 00:56:14 +0400

    Added IsRunning property to AsyncProcessor.

--- InoSoft.Tools/AsyncProcessor.cs
+++ InoSoft.Tools/AsyncProcessor.cs
@@ -6,15 +6,32 @@ namespace InoSoft.Tools
 {
     public abstract class AsyncProcessor<T>
     {
-        private Thread _dispatcherThread;
-        private readonly EventWaitHandle _queueHasItemsEvent;
         private readonly Queue<T> _queue;
+        private readonly EventWaitHandle _queueHasItemsEvent;
+        private Thread _dispatcherThread;
         private bool _isRunning;
 
         protected AsyncProcessor()
         {
             _queue = new Queue<T>();
             _queueHasItemsEvent = new EventWaitHandle(false, EventResetMode.ManualReset);
+        }
+
+        public bool IsRunning
+        {
+            get { return _isRunning; }
+        }
+
+        public void EnqueueItem(T item)
+        {
+            lock (_queue)
+            {
+                if (_isRunning)
+                {
+                    _queue.Enqueue(item);
+                    _queueHasItemsEvent.Set();
+                }
+            }
         }
 
         public void Start()
@@ -35,23 +52,11 @@ namespace InoSoft.Tools
             }
         }
 
-        public void EnqueueItem(T item)
-        {
-            lock (_queue)
-            {
-                if (_isRunning)
-                {
-                    _queue.Enqueue(item);
-                    _queueHasItemsEvent.Set();
-                }
-            }
-        }
-
-        protected abstract void ProcessItem(T item);
-
         protected virtual void OnProcessItemException(T item, Exception ex)
         {
         }
+
+        protected abstract void ProcessItem(T item);
 
         private void RunDispatcher()
         {
