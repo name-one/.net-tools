fdbf52a7abb7a38b068beadd154bcb846aeb058c
Alexey Ostrovsky <d4ruko@gmail.com>
2012-07-26 12:27:42 +0400

    Added UpdateProperties() method to ReflectionHelper.

--- InoSoft.Tools/ReflectionHelper.cs
+++ InoSoft.Tools/ReflectionHelper.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Reflection;
 
 namespace InoSoft.Tools
@@ -62,6 +63,44 @@ namespace InoSoft.Tools
                 result[i] = Clone<TSource, TDest>(source[i]);
             }
             return result;
+        }
+
+        /// <summary>
+        /// Compares property values of two objects and sets values from source to destination if they differ.
+        /// </summary>
+        /// <param name="dest">Destination object.</param>
+        /// <param name="source">Source object.</param>
+        /// <param name="propertySelector">
+        /// Function returning true for those properties that need to be updated.
+        /// By default, all the properties are updated.
+        /// </param>
+        /// <returns>Array of PropertyInfo instances representing those destination properties that were changed.</returns>
+        public static PropertyInfo[] UpdateProperties(object dest, object source,
+            Func<PropertyInfo, bool> propertySelector = null)
+        {
+            if (source == null)
+                return new PropertyInfo[0];
+            var result = new List<PropertyInfo>();
+            Type sourceType = source.GetType();
+            Type destType = dest.GetType();
+            foreach (var sourceProp in sourceType.GetProperties())
+            {
+                var property = destType.GetProperty(sourceProp.Name);
+                if (property == null || !property.CanWrite)
+                    continue;
+                var sourcePropertyValue = sourceProp.GetValue(source, null);
+                if (sourcePropertyValue == null)
+                    continue;
+                var destPropertyValue = property.GetValue(dest, null);
+                if ((propertySelector == null || propertySelector(property))
+                    && !destPropertyValue.MemberwiseEquals(sourcePropertyValue))
+                {
+                    property.SetValue(dest, sourcePropertyValue, null);
+                    result.Add(property);
+                }
+            }
+
+            return result.ToArray();
         }
     }
 }
