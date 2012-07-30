4af93431f9c6c65f8952749031e4f9ac68e9cc2c
Kirill Gorobets <kgorob@gmail.com>
2012-07-30 17:08:01 +0000

    Custom event handler extensions

--- InoSoft.Tools/CustomEventHandler.cs
+++ InoSoft.Tools/CustomEventHandler.cs
@@ -1,4 +1,15 @@
 ﻿namespace InoSoft.Tools
 {
     public delegate void CustomEventHandler<TSender, TArg>(TSender sender, TArg arg);
+
+    public static class CustomEventHandlerExtensions
+    {
+        public static void Raise<TSender, TArgs>(this CustomEventHandler<TSender, TArgs> eventHandler, TSender sender, TArgs args)
+        {
+            if (eventHandler != null)
+            {
+                eventHandler(sender, args);
+            }
+        }
+    }
 }
