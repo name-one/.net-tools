e051903b637fe8f91e5b3d35830f944876322d8e
Alexey Ostrovsky <d4ruko@gmail.com>
2012-09-04 17:13:54 +0400

    Added stream support to XmlHelper.

--- InoSoft.Tools/XmlHelper.cs
+++ InoSoft.Tools/XmlHelper.cs
@@ -6,22 +6,28 @@ namespace InoSoft.Tools
 {
     public static class XmlHelper
     {
-        public static T LoadObjectFromFile<T>(string path)
+        /// <summary>
+        /// Deserializes an object from an XML data stream.
+        /// </summary>
+        /// <typeparam name="T">Type of the object to deserialize.</typeparam>
+        /// <param name="stream">Stream to deserialize object from.</param>
+        /// <exception cref="ArgumentException" />
+        /// <exception cref="ArgumentNullException" />
+        /// <exception cref="InvalidOperationException" />
+        /// <exception cref="InvalidDataException" />
+        public static T FromXml<T>(Stream stream)
+            where T : class
         {
-            using (FileStream fileStream = new FileStream(path, FileMode.Open))
-            {
-                return (T)new XmlSerializer(typeof(T)).Deserialize((Stream)fileStream);
-            }
-        }
+            if (stream == null)
+                throw new ArgumentNullException("stream");
 
-        public static void SaveObjectToFile<T>(T obj, string path)
-        {
-            XmlSerializerNamespaces namespaces = new XmlSerializerNamespaces();
-            namespaces.Add("", "");
-            using (FileStream fileStream = new FileStream(path, FileMode.Create))
+            var ser = new XmlSerializer(typeof(T));
+            var item = ser.Deserialize(stream) as T;
+            if (item == null)
             {
-                new XmlSerializer(typeof(T)).Serialize((Stream)fileStream, (object)obj, namespaces);
+                throw new InvalidDataException(String.Format("The file must contain a {0} instance.", typeof(T).Name));
             }
+            return item;
         }
 
         /// <summary>
@@ -29,28 +35,45 @@ namespace InoSoft.Tools
         /// </summary>
         /// <typeparam name="T">Type of the object to deserialize.</typeparam>
         /// <param name="path">Path of the file to deserialize object from.</param>
-        /// <exception cref="ArgumentException"></exception>
-        /// <exception cref="ArgumentNullException"></exception>
-        /// <exception cref="IOException"></exception>
-        /// <exception cref="FileNotFoundException"></exception>
-        /// <exception cref="PathTooLongException"></exception>
-        /// <exception cref="UnauthorizedAccessException"></exception>
-        /// <exception cref="InvalidOperationException"></exception>
-        /// <exception cref="InvalidDataException"></exception>
-        /// <exception cref="NotSupportedException"></exception>
+        /// <exception cref="ArgumentException" />
+        /// <exception cref="ArgumentNullException" />
+        /// <exception cref="IOException" />
+        /// <exception cref="FileNotFoundException" />
+        /// <exception cref="PathTooLongException" />
+        /// <exception cref="UnauthorizedAccessException" />
+        /// <exception cref="InvalidOperationException" />
+        /// <exception cref="InvalidDataException" />
+        /// <exception cref="NotSupportedException" />
         public static T FromXml<T>(string path)
             where T : class
         {
+            if (path == null)
+                throw new ArgumentNullException("path");
+
             using (var file = File.OpenRead(path))
             {
-                var ser = new XmlSerializer(typeof(T));
-                var item = ser.Deserialize(file) as T;
-                if (item == null)
-                {
-                    throw new InvalidDataException(String.Format("The file must contain a {0} instance.", typeof(T).Name));
-                }
-                return item;
+                return FromXml<T>(file);
             }
+        }
+
+        /// <summary>
+        /// Serializes an object to an XML data stream.
+        /// </summary>
+        /// <typeparam name="T">Type of the object to serialize.</typeparam>
+        /// <param name="item">Object to serialize</param>
+        /// <param name="stream">Stream to write the serialized object to.</param>
+        /// <exception cref="ArgumentException" />
+        /// <exception cref="ArgumentNullException" />
+        /// <exception cref="InvalidOperationException" />
+        public static void ToXml<T>(T item, Stream stream)
+        {
+            if (stream == null)
+                throw new ArgumentNullException("stream");
+
+            var serializer = new XmlSerializer(typeof(T));
+            var namespaces = new XmlSerializerNamespaces();
+            namespaces.Add("", "");
+            serializer.Serialize(stream, item, namespaces);
         }
 
         /// <summary>
@@ -60,17 +83,19 @@ namespace InoSoft.Tools
         /// <typeparam name="T">Type of the object to serialize.</typeparam>
         /// <param name="item">Object to serialize</param>
         /// <param name="path">Path of the file to save the serialized object to.</param>
-        /// <exception cref="ArgumentException"></exception>
-        /// <exception cref="ArgumentNullException"></exception>
-        /// <exception cref="IOException"></exception>
-        /// <exception cref="DirectoryNotFoundException"></exception>
-        /// <exception cref="PathTooLongException"></exception>
-        /// <exception cref="UnauthorizedAccessException"></exception>
-        /// <exception cref="InvalidOperationException"></exception>
-        /// <exception cref="NotSupportedException"></exception>
+        /// <exception cref="ArgumentException" />
+        /// <exception cref="ArgumentNullException" />
+        /// <exception cref="IOException" />
+        /// <exception cref="DirectoryNotFoundException" />
+        /// <exception cref="PathTooLongException" />
+        /// <exception cref="UnauthorizedAccessException" />
+        /// <exception cref="InvalidOperationException" />
+        /// <exception cref="NotSupportedException" />
         public static void ToXml<T>(T item, string path)
-            where T : class
         {
+            if (path == null)
+                throw new ArgumentNullException("path");
+
             var directory = Path.GetDirectoryName(path);
             if (directory != null && !Directory.Exists(directory))
             {
@@ -78,11 +103,44 @@ namespace InoSoft.Tools
             }
             using (var file = File.Create(path))
             {
-                var serializer = new XmlSerializer(typeof(T));
-                var namespaces = new XmlSerializerNamespaces();
-                namespaces.Add("", "");
-                serializer.Serialize(file, item, namespaces);
+                ToXml(item, file);
             }
         }
+
+        #region Obsolete
+
+        /// <summary>
+        /// Deserializes an object from an XML file with specified path.
+        /// </summary>
+        /// <typeparam name="T">Type of the object to deserialize.</typeparam>
+        /// <param name="path">Path of the file to deserialize object from.</param>
+        [Obsolete("Use XmlHelper.FromXml<T>(string) instead.")]
+        public static T LoadObjectFromFile<T>(string path)
+        {
+            using (var fileStream = new FileStream(path, FileMode.Open))
+            {
+                return (T)new XmlSerializer(typeof(T)).Deserialize(fileStream);
+            }
+        }
+
+        /// <summary>
+        /// Serializes an object to an XML file with specified path. If the file and/or directory does not exist, creates them.
+        /// If the file already exists, overwrites it.
+        /// </summary>
+        /// <typeparam name="T">Type of the object to serialize.</typeparam>
+        /// <param name="obj">Object to serialize</param>
+        /// <param name="path">Path of the file to save the serialized object to.</param>
+        [Obsolete("Use ToXml<T>(T, string) instead.")]
+        public static void SaveObjectToFile<T>(T obj, string path)
+        {
+            var namespaces = new XmlSerializerNamespaces();
+            namespaces.Add("", "");
+            using (var fileStream = new FileStream(path, FileMode.Create))
+            {
+                new XmlSerializer(typeof(T)).Serialize(fileStream, obj, namespaces);
+            }
+        }
+
+        #endregion Obsolete
     }
 }
